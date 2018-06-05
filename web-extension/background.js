8be21b37e7db76f34c309fef964ae0767b494a4a
zsau <zsau@fastmail.com>
2018-06-04 21:43:52 -0700

    show error as notification if popup is closed; revert clipboard stuff to popup for now

--- web-extension/background.js
+++ web-extension/background.js
@@ -1,49 +1,27 @@
 'use strict';
 
-function onLoginCredentialsDoCopyClipboard(response, tabId) {
-    browser.tabs.sendMessage(tabId, {
-        type: 'COPY_PASSWORD',
-        password: response.password,
-    });
-}
-
-function onLoginCredentialsDoLogin(response, tabId, url) {
-    if (response.username === urlDomain(url)) {
-        logAndDisplayError(i18n.getMessage('couldNotDetermineUsernameMessage'), tabId);
-        return;
-    }
-
-    browser.tabs.sendMessage(tabId, {
-        type: 'FILL_LOGIN_FIELDS',
-        login: response.username,
-        password: response.password,
-    });
-
-    executeOnSetting('submitafterfill', () => {
-        browser.tabs.sendMessage(tabId, { type: 'TRY_LOGIN' });
-    });
-}
-
-function logAndDisplayError(error, tabId) {
-    console.log(error);
-    browser.tabs.sendMessage(tabId, {
-        type: 'SHOW_ERROR',
-        error: error,
-    });
-}
-
 function processMessage(message, sender, sendResponse) {
     const { entry, tabId, url, copyOnly } = message;
 
-    sendNativeAppMessage({ type: 'getLogin', entry: entry }).then(
-        response => {
-            if (copyOnly) onLoginCredentialsDoCopyClipboard(response, tabId);
-            else onLoginCredentialsDoLogin(response, tabId, url);
-        },
-        error => {
-            logAndDisplayError(error, tabId);
-        }
-    );
+    return sendNativeAppMessage({ type: 'getLogin', entry: entry })
+        .then(response => {
+            if (response.error) throw new Error(response.error);
+            else if (response.username === urlDomain(url))
+                throw new Error(i18n.getMessage('couldNotDetermineUsernameMessage'));
+
+            if (copyOnly) return response;
+            else
+                return browser.tabs.sendMessage(tabId, {
+                    type: 'FILL_LOGIN_FIELDS',
+                    login: response.username,
+                    password: response.password,
+                });
+        })
+        .catch(error => {
+            const popups = browser.extension.getViews({ type: 'popup' });
+            if (popups.length === 0) showNotificationOnSetting(error.message);
+            throw error;
+        });
 }
 
 function initBackground() {
