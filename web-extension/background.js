bf53d0c854b6d7c5f65c480d1cd2bdd3f94d4f5d
Martin Hoefling <martin.hoefling@gmx.de>
2018-06-05 22:12:01 +0200

    Refactored for future message types, still needs tests and test fixes

--- web-extension/background.js
+++ web-extension/background.js
@@ -1,31 +1,52 @@
 'use strict';
 
-function processMessage(message, sender, sendResponse) {
-    const { entry, tabId, url, copyOnly } = message;
+function _processMessage(message, sender, sendResponse) {
+    if (sender.tab) {
+        throw new Error(`Background script received unexpected message ${JSON.dumps(message)} from content script.`);
+    }
 
-    return sendNativeAppMessage({ type: 'getLogin', entry: entry })
-        .then(response => {
-            if (response.error) throw new Error(response.error);
-            else if (response.username === urlDomain(url))
-                throw new Error(i18n.getMessage('couldNotDetermineUsernameMessage'));
+    const { entry, tab } = message;
+    switch (message.type) {
+        case 'LOGIN_TAB':
+            return sendNativeAppMessage({ type: 'getLogin', entry: entry }).then(response => {
+                if (response.error) {
+                    sendResponse(response);
+                    throw new Error(response.error);
+                }
 
-            if (copyOnly) return response;
-            else
-                return browser.tabs.sendMessage(tabId, {
-                    type: 'FILL_LOGIN_FIELDS',
-                    login: response.username,
-                    password: response.password,
-                });
-        })
-        .catch(error => {
-            const popups = browser.extension.getViews({ type: 'popup' });
-            if (popups.length === 0) showNotificationOnSetting(error.message);
-            throw error;
-        });
+                if (response.username === urlDomain(tab.url)) {
+                    const msg = i18n.getMessage('couldNotDetermineUsernameMessage');
+                    sendResponse({ error: msg });
+                    throw new Error(msg);
+                }
+
+                return browser.tabs
+                    .sendMessage(tab.id, {
+                        type: 'FILL_LOGIN_FIELDS',
+                        login: response.username,
+                        password: response.password,
+                    })
+                    .then(() => {
+                        return executeOnSetting('submitafterfill', () => {
+                            browser.tabs.sendMessage(tab.id, { type: 'TRY_LOGIN' });
+                        });
+                    });
+            });
+
+        default:
+            throw new Error(`Background script received unexpected message ${JSON.dumps(message)} from extension`);
+    }
+}
+
+function processMessageAndCatch(message, sender, sendResponse) {
+    return _processMessage(message, sender, sendResponse).catch(error => {
+        showNotificationOnSetting(error.message);
+        throw error;
+    });
 }
 
 function initBackground() {
-    browser.runtime.onMessage.addListener(processMessage);
+    browser.runtime.onMessage.addListener(processMessageAndCatch);
 }
 
 initBackground();
@@ -33,5 +54,6 @@ initBackground();
 window.tests = {
     background: {
         initBackground,
+        processMessageAndCatch,
     },
 };
