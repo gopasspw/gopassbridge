cadbbd5e4779e63a8ef80c6748ec2029fe29201c
Martin Hoefling <martin.hoefling@gmx.de>
2019-07-07 13:37:09 +0200

    Apply prettier

--- web-extension/details.js
+++ web-extension/details.js
@@ -1,111 +1,113 @@
-'use strict'
+'use strict';
 
 function onEntryData(element, message) {
-    let alreadyShown = false
+    let alreadyShown = false;
     Array.from(document.getElementsByClassName('detail-view')).forEach(oldDetailView => {
         if (oldDetailView.previousSibling === element) {
-            alreadyShown = true
+            alreadyShown = true;
         }
-        oldDetailView.remove()
-    })
+        oldDetailView.remove();
+    });
     return browser.storage.local.remove(LAST_DETAIL_VIEW_PREFIX + urlDomain(currentPageUrl)).then(() => {
         if (alreadyShown) {
-            return
+            return;
         }
 
-        const newDetailView = _detailViewFromMessage(message)
-        newDetailView.classList.add('detail-view')
-        _insertAfter(newDetailView, element)
-        setLocalStorageKey(LAST_DETAIL_VIEW_PREFIX + urlDomain(currentPageUrl), element.innerText)
-    })
+        const newDetailView = _detailViewFromMessage(message);
+        newDetailView.classList.add('detail-view');
+        _insertAfter(newDetailView, element);
+        setLocalStorageKey(LAST_DETAIL_VIEW_PREFIX + urlDomain(currentPageUrl), element.innerText);
+    });
 }
 
 function _insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 function _detailViewFromMessage(message) {
-    const container = document.createElement('div')
-    Object.keys(message).filter(key => message[key] !== null && message[key] !== undefined && message[key] !== '').forEach(key => _appendEntry(container, key, message[key]))
-    return container
+    const container = document.createElement('div');
+    Object.keys(message)
+        .filter(key => message[key] !== null && message[key] !== undefined && message[key] !== '')
+        .forEach(key => _appendEntry(container, key, message[key]));
+    return container;
 }
 
 function _createNestedValueElement(value) {
-    const valueElement = document.createElement('div')
-    valueElement.classList.add('detail-nested')
-    Object.keys(value).forEach(key => _appendEntry(valueElement, key, value[key]))
-    return valueElement
+    const valueElement = document.createElement('div');
+    valueElement.classList.add('detail-nested');
+    Object.keys(value).forEach(key => _appendEntry(valueElement, key, value[key]));
+    return valueElement;
 }
 
 function _createURLValueElement(value) {
-    const valueElement = document.createElement('a')
-    valueElement.href = value.match(re_weburl)[0]
-    valueElement.target = '_blank'
-    valueElement.innerText = value.match(re_weburl)[0]
-    valueElement.addEventListener('click', openURL)
-    return valueElement
+    const valueElement = document.createElement('a');
+    valueElement.href = value.match(re_weburl)[0];
+    valueElement.target = '_blank';
+    valueElement.innerText = value.match(re_weburl)[0];
+    valueElement.addEventListener('click', openURL);
+    return valueElement;
 }
 
 function _createSimpleValueElement(value) {
-    const valueElement = document.createElement('span')
-    valueElement.innerText = value
-    valueElement.addEventListener('click', _copyElementToClipboard)
-    return valueElement
+    const valueElement = document.createElement('span');
+    valueElement.innerText = value;
+    valueElement.addEventListener('click', _copyElementToClipboard);
+    return valueElement;
 }
 
 function _isNested(value) {
-    return value !== null && typeof value === 'object'
+    return value !== null && typeof value === 'object';
 }
 
 function _isURL(value) {
-    return typeof value === 'string' && value.match(re_weburl)
+    return typeof value === 'string' && value.match(re_weburl);
 }
 
 function _createFlatValueElement(value) {
-    const valueElement = _isURL(value) ? _createURLValueElement(value) : _createSimpleValueElement(value)
-    valueElement.classList.add('detail-clickable-value')
-    return valueElement
+    const valueElement = _isURL(value) ? _createURLValueElement(value) : _createSimpleValueElement(value);
+    valueElement.classList.add('detail-clickable-value');
+    return valueElement;
 }
 
 function _appendEntry(container, key, value) {
-    let keyElement, valueElement
-    const entryElement = document.createElement('li')
+    let keyElement, valueElement;
+    const entryElement = document.createElement('li');
 
-    keyElement = document.createElement('span')
-    keyElement.innerText = `${key}:`
-    keyElement.classList.add('detail-key')
-    valueElement = _isNested(value) ? _createNestedValueElement(value) : _createFlatValueElement(value)
+    keyElement = document.createElement('span');
+    keyElement.innerText = `${key}:`;
+    keyElement.classList.add('detail-key');
+    valueElement = _isNested(value) ? _createNestedValueElement(value) : _createFlatValueElement(value);
 
-    entryElement.appendChild(keyElement)
-    entryElement.appendChild(valueElement)
-    container.appendChild(entryElement)
+    entryElement.appendChild(keyElement);
+    entryElement.appendChild(valueElement);
+    container.appendChild(entryElement);
 }
 
 function _copyElementToClipboard(event) {
-    const element = event.target
-    copyToClipboard(element.innerText)
+    const element = event.target;
+    copyToClipboard(element.innerText);
 }
 
 function restoreDetailView() {
-    let element
+    let element;
     return getLocalStorageKey(LAST_DETAIL_VIEW_PREFIX + urlDomain(currentPageUrl)).then(value => {
         if (!value) {
-            return Promise.resolve()
+            return Promise.resolve();
         }
 
         Array.from(document.getElementsByClassName('login')).forEach(loginElement => {
             if (loginElement.innerText === value) {
-                element = loginElement
+                element = loginElement;
             }
-        })
+        });
         if (element) {
             return sendNativeAppMessage({ type: 'getData', entry: value }).then(
                 message => onEntryData(element, message),
                 logAndDisplayError
-            )
+            );
         }
-        return Promise.resolve()
-    })
+        return Promise.resolve();
+    });
 }
 
 window.tests = {
