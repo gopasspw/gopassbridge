b0a3e002314003ce73be3809e54089776ec42a02
Patrick Harböck <patrick.harboeck@tngtech.com>
2026-02-07 17:55:50 +0100

    Add E2E test for custom options
    Signed-off-by: Patrick Harböck <patrick.harboeck@tngtech.com>

--- tests/e2e/options.spec.mjs
+++ tests/e2e/options.spec.mjs
@@ -7,3 +7,20 @@ test.beforeEach(async ({ page, extensionId }) => {
 test('options page loads successfully', async ({ page }) => {
     await expect(page).toHaveTitle('gopass bridge settings');
 });
+
+test('can set custom options', async ({ page }) => {
+    const passwordLengthInput = page.locator('#defaultpasswordlength');
+    const defaultFolderInput = page.locator('#defaultfolder');
+
+    await passwordLengthInput.fill('100');
+    await defaultFolderInput.fill('TestFolder');
+
+    const savingIndicator = page.locator('#savingindicator');
+    await expect(savingIndicator).toHaveClass(/saved/);
+    await expect(savingIndicator).not.toHaveClass(/saved/);
+
+    await page.reload();
+
+    await expect(passwordLengthInput).toHaveValue('100');
+    await expect(defaultFolderInput).toHaveValue('TestFolder');
+});
